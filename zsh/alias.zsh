ebcf1efbe69173e5cae879b17c0c4b809f7aace3
robertsimoes <robertsimoes@users.noreply.github.com>
2020-10-24 10:45:24 -0700

    Splits ZSH into alias and RC

---
+++ zsh/alias.zsh
@@ -0,0 +1,45 @@
+
+# dirs
+alias \@badtb='cd ~/dev/book/badtb-app'
+alias \@tradebaybot='cd ~/dev/tradebay-bot-go'
+
+# vim
+alias vimold='vim'
+alias v='vim'
+alias vim='nvim'
+
+# Warn before overwriting
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Docker
+alias d='docker'
+alias dps='docker ps'
+alias dockerterm='docker exec -it'
+alias dockerbuild='docker build -t'
+alias dockeripd="docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
+
+# python
+alias python=python3
+alias pip=pip3
+
+# configs
+alias ,ez="vim ~/.zshrc"
+alias ,ev="vim ~/.vimrc"
+alias ,et="vim ~/.tmux.conf"
+
+# Terminal
+alias cl="clear"
+alias reload="source ~/.zshrc"
+
+# git
+alias ga="git add -A"
+alias gcm="git commit -m"
+alias gbr="git branch | cat"
+alias gf="git commit --fixup"
+alias grename="git branch -m"
+
+# Youtube
+alias youtube='youtube-dl --extract-audio --audio-format mp3'
+
