1a20ef5394d626d83b6f8d6ae8c5ed4bac5f7f6c
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-09-05 08:50:00 -0700

    [ZSH] Docker functions

--- zsh/func.zsh
+++ zsh/func.zsh
@@ -7,3 +7,22 @@ cfu() {
     git commit --fixup $(echo $target)
   fi
 }
+
+docker-start() {
+    docker-machine start default
+    eval $(docker-machine env default)
+}
+
+docker-stop() {
+    docker-machine stop default
+}
+
+_docker() {
+	if [ -z "${DOCKER_HOST}" ]; then
+		eval $(docker-machine env)
+	fi
+
+	docker "$@"
+}
+alias docker=_docker
+alias k=kubectl
