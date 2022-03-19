8bcb1e1a990af0e37d68bf0cad1fcb3fea7269f0
Robert <16901597+aquaflamingo@users.noreply.github.com>
2022-03-19 11:06:49 -0700

    Profiles and removes default nvm loading to speed up zsh

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,4 +1,7 @@
-# Language specific environment
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+function load_nvm() {
+	 # Language specific environment
+	 export NVM_DIR="$HOME/.nvm"
+	[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+	[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+}
+
