c1a0bf02b43808bff9d0089249090155280f33b5
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-04-12 21:43:39 -0700

    Bufferline config

---
+++ nvim/lua/user/bufferline.lua
@@ -0,0 +1,93 @@
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+  return
+end
+
+bufferline.setup {
+  options = {
+    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    -- NOTE: this plugin is designed with this icon in mind,
+    -- and so changing this is NOT recommended, this is intended
+    -- as an escape hatch for people who cannot bear it for whatever reason
+    indicator_icon = "▎",
+    -- buffer_close_icon = '',
+    modified_icon = "●",
+    max_name_length = 30,
+    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+    tab_size = 21,
+    diagnostics = false, -- | "nvim_lsp" | "coc",
+    diagnostics_update_in_insert = false,
+    show_buffer_icons = false,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    show_tab_indicators = true,
+    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+  },
+  highlights = {
+    fill = {
+      guifg = { attribute = "fg", highlight = "#ff0000" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+    background = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+
+    buffer_visible = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+    tab_selected = {
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    tab = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+    tab_close = {
+      -- guifg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
+      guifg = { attribute = "fg", highlight = "TabLineSel" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    duplicate_selected = {
+      guifg = { attribute = "fg", highlight = "TabLineSel" },
+      guibg = { attribute = "bg", highlight = "TabLineSel" },
+      gui = "italic",
+    },
+    duplicate_visible = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+      gui = "italic",
+    },
+    duplicate = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+      gui = "italic",
+    },
+    modified = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+    modified_selected = {
+      guifg = { attribute = "fg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    modified_visible = {
+      guifg = { attribute = "fg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+    separator = {
+      guifg = { attribute = "bg", highlight = "TabLine" },
+      guibg = { attribute = "bg", highlight = "TabLine" },
+    },
+    separator_selected = {
+      guifg = { attribute = "bg", highlight = "Normal" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+    indicator_selected = {
+      guifg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+      guibg = { attribute = "bg", highlight = "Normal" },
+    },
+  },
+}
