f19a7db416425d77b155b4559af0ffadd95a8410
aquaflamingos <16901597+aquaflamingo@users.noreply.github.com>
2026-01-03 19:16:29 -0500

    fixup! Refactor to 2026

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -2,24 +2,29 @@
 -- Other utility plugins
 
 return {
-  -- Smoothly navigate between tmux and Neovim panes
-  {
-    "alexghergh/nvim-tmux-navigation",
-    lazy = true,
-    config = function()
-      require("nvim-tmux-navigation").setup({
-        disable_when_zoomed = true, -- defaults to false
-      })
-    end,
-  },
+	-- Smoothly navigate between tmux and Neovim panes
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local ntn = require("nvim-tmux-navigation")
+			ntn.setup({
+				disable_when_zoomed = true, -- matches your preference
+			})
+			-- tmux "sends" the keys back to it
+			vim.keymap.set("n", "<C-h>", ntn.NvimTmuxNavigateLeft)
+			vim.keymap.set("n", "<C-j>", ntn.NvimTmuxNavigateDown)
+			vim.keymap.set("n", "<C-k>", ntn.NvimTmuxNavigateUp)
+			vim.keymap.set("n", "<C-l>", ntn.NvimTmuxNavigateRight)
+		end,
+	},
 
-  -- An optional companion plugin for copilot.lua
-  {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "zbirenbaum/copilot.lua",
-    },
-    lazy = true,
-  },
+	-- -- An optional companion plugin for copilot.lua
+	-- {
+	--   "olimorris/codecompanion.nvim",
+	--   dependencies = {
+	--     "nvim-lua/plenary.nvim",
+	--     "zbirenbaum/copilot.lua",
+	--   },
+	--   lazy = true,
+	-- },
 }
